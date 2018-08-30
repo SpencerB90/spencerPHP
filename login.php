3b658152ab40f3c5ceb6b2300c9db09db5192596
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-08-30 10:41:52 -0400

    testing

--- login.php
+++ login.php
@@ -4,10 +4,26 @@
     <meta charset="utf-8">
     <title></title>
   </head>
+
+<?php
+$username = $_GET['username'];
+$password = $_GET['password'];
+ ?>
+
   <body>
     <form method="get" action="">
-      <input type="text" name="first"> <br />
-      <input type="text" name="last">
+      <input type="text" name="username" placeholder="enter username"> <br />
+      <input type="password" name="Password" >
+      <br>
+      <input type="submit" value="go">
     </form>
+
+<?php
+echo "username was " + $username;
+echo "<br>";
+echo "password was" + $password;
+
+ ?>
+
   </body>
 </html>
