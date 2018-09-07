a46f3400aeb36a573898c450fd9ae713c4e2ee7d
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-09-07 10:50:05 -0400

    test

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+//must be in caps!
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  require('dbConnect.php');
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $sql = "INSERT INTO users (username,password) VALUES ('$username','$password')";
+  $conn->query($sql);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form action="" method="post">
+      <input type="text" name"username"><br>
+      <input type="password" name="password"><br>
+      <input type="sumbit">
+    </form>
+
+  </body>
+</html>
