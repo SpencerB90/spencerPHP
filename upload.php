a7965c510ec27936dce4c82b8ff0dabaf1dd8c72
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-09-12 11:25:27 -0400

    test

--- upload.php
+++ upload.php
@@ -7,10 +7,17 @@ if (!isset($_SESSION)) {
 if (!isset($_SESSION['username'])) {
   //die("Don't even try mate");
   //before html cant but put at bottom, must be at top
-  header('login.php');
+  //when you use header you need 'location: then where you are going to'
+  header('Location: login.php'); //if you wanted https address you need full url
 }
+//takes whatever this is and tells you about it, good for trouble shooting
+var_dump($_FILES['upload']);
+
+echo"<hr />";
+var_dump($_POST['upload']); //trouble shooting wrong statement
+
 //code for uploading file, will work after post data is sent
-if (isset($_POST['upload'])){
+if (isset($_FILES['upload']) ){ //could use != null after ] instead of isset
   $target_dir = "uploads/";
   $target_file = $target_dir . basename($_FILES['upload']['name']);
   //moves files
