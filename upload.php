f204d3199cb7996833335402c4fc0f5885fbe0ec
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-09-19 11:30:19 -0400

    test

--- upload.php
+++ upload.php
@@ -28,7 +28,7 @@ if (isset($_FILES['upload']) ){ //could use != null after ] instead of isset
   }
 
   //creates file for individual user
-  if (!file_exists(("uploads/" . $_SESSION['username']))) {
+  if (!file_exists("uploads/" . $_SESSION['username'])) {
     mkdir("uploads/" . "$_SESSION['username']");
   }
 
