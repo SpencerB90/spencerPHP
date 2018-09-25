d3621474210fc839239e654bbf00bdeaa55a22b0
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-09-25 10:44:03 -0400

    test

--- edituser.php
+++ edituser.php
@@ -24,8 +24,11 @@ if (isset($_GET['id']) && $_GET['edit']=="edit"){
   echo "<input name=\"username\" type=\"text\" value=\"" . $row['username'] . "\">";
   echo"<br />";
   echo "<input name=\"password\" type=\"text\" value=\"" . $row['password'] . "\">";
-
+  echo"<br />";
+  echo "<input type=\"submit\" name=\"submit\" value=\"change\">";
   }
+  
+echo "</form>";
 
 }
 else {
