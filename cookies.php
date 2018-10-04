3b3b2f3bb3a18b6fef030b43d6ea7b919e0fd5e5
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-10-04 10:14:17 -0400

    test

---
+++ cookies.php
@@ -0,0 +1,30 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "bob";
+
+//setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+//86400 = 1 day
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+
+     <?php
+
+      if (isset($_COOKIE['user'])){
+        echo "you have been here before";
+      }
+      else{
+        echo "this is your first time here";
+      }
+
+      ?>
+
+   </body>
+ </html>
