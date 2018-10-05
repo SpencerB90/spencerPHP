973bb4bd44381f97e49022c192a3f8715e1404d1
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-10-05 09:27:56 -0400

    test

--- cookies.php
+++ cookies.php
@@ -1,9 +1,12 @@
 <?php
-$cookie_name = "user";
-$cookie_value = "bob";
 
-//setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-//86400 = 1 day
+session_start();
+require('dbConnect.php');
+
+$cookie_name = $username;
+$cookie_value = "here";
+
+
 
  ?>
 
@@ -17,19 +20,29 @@ $cookie_value = "bob";
 
      <?php
 
-      if (isset($_COOKIE['user'])){
-        echo "Before have you been";
+      if (isset($_COOKIE[$username])){
+        $last = $_COOKIE[$username]; }
+        $month = (86400 * 30) + time();
+        //this adds one year to the current time, for the cookie expiration
+        setcookie(AboutVisit, time (), $month) ;
+        if (isset ($last))
+        {
+        $change = time () - $last;
+        if ( $change > 86400 - 24)
+        {
+        echo "Welcome back! <br> You last visited on ". date("m/d/y",$last) ;
+        // Tells the user when they last visited if it was over a day ago
       }
       else{
         echo "Here first time, yes";
         //can run after html?
-      // setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+        setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
         //86400 = 1 day
-
-        setcookie($cookie_name, $cookie_value, time() + (60), "/");
       }
 
       ?>
+
+
 
    </body>
  </html>
