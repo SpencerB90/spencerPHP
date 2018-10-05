d7715c52ec97e4e0746dd64ced042df31004bb5d
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-10-05 09:45:39 -0400

    test

--- cookies.php
+++ cookies.php
@@ -1,1 +1,19 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "pete";
+ ?>
 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+      
+
+
+
+  </body>
+</html>
