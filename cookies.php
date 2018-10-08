18ccfd8231c390dc79bc074545f08fe885ba2ef9
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-10-08 19:35:28 -0400

    test

--- cookies.php
+++ cookies.php
@@ -18,7 +18,7 @@ $cookie_value = "pete";
          date_default_timezone_set('America/New_York');
          $visit = $_COOKIE['lastVisit'];
          $now = date();
-         $then =  date($visit);
+         $then =  $visit;
 
          echo "Welcome back! <br> You last visited on " . $visit;
          // Tells the user when they last visited if it was over a day ago
