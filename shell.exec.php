ef852cd32008c9d6091d801b14cc8ed66acf6b70
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-10-10 11:44:55 -0400

    test

--- shell.exec.php
+++ shell.exec.php
@@ -9,22 +9,6 @@ echo "<pre>$pwd</pre>";
 ?>
 
 
-<?php
-$filename = '/var/www/html/spencer/spencerPHP/test';
-
-//checks for files and directories
-
-//isdir
-
-if (file_exists($filename)) {
-    echo "The file $filename";
-    echo "<br>- exists";
-} else {
-    echo "The file $filename";
-      echo "<br>- does not exist";
-}
-?>
-
 
 <?php
 $folder = file_exists("test");
