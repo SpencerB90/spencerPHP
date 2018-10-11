96570a5194fe5f46aefa1d4b3bd33adfd2cf474d
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-10-11 10:47:56 -0400

    test

--- shell.exec.php
+++ shell.exec.php
@@ -40,41 +40,8 @@ else{
 }
 ?>
 
-<?php
-if ($_SESSION["UserID"]!="" && $_SESSION["UserID"]!="Guest") {
-    $sql = "update users set lastaccess=now() where username='".$_SESSION["UserID"]."'";
-    CustomQuery($sql);
-}
 
- ?>
- <?php
 
- $minutes=10;
- $dispUsers=20;
- $t=date('Y-m-d H:i:s', time()-$minutes*60);
- // display users who were active in last 10 minutes
-
- $users=DBLookup("select count(*) from invusers where lastaccess > '".$t."'");
- if ($users>0) {
-     $sql="select * from invusers where lastaccess > '".$t."'";  
-     $rs=CustomQuery($sql);
-
-     if ($data = db_fetch_array($rs)) {
-         echo $users." active user(s): ".$data["username"];
-     }
-     $count=1;
-     while ($data = db_fetch_array($rs)) {
-       if ($count<$dispUsers) {
-             $count++;
-             echo ", ".$data["username"];
-       }
-         else {
-             echo " ...";
-             break;
-         }
-     }
- }
- ?>
 
 
 <!-- $filename = '/var/www/html/spencer/spencerPHP/test';
