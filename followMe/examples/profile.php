37c05db092ee50b766f9f0b0272932d58375c359
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-10-23 10:28:20 -0400

    test

--- followMe/examples/profile.php
+++ followMe/examples/profile.php
@@ -60,15 +60,15 @@ require('dbConnect.php');
 </head>
 <body>
     <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
-        <div class="container">
-			<div class="navbar-translate">
-	            <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-bar"></span>
-					<span class="navbar-toggler-bar"></span>
-					<span class="navbar-toggler-bar"></span>
-	            </button>
-	            <a class="navbar-brand" href="#">Profile</a>
-			</div>
+      <div class="container">
+			  <div class="navbar-translate">
+	        <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+					  <span class="navbar-toggler-bar"></span>
+					  <span class="navbar-toggler-bar"></span>
+				  	<span class="navbar-toggler-bar"></span>
+	        </button>
+	        <a class="navbar-brand" href="#">Profile</a>
+			  </div>
 			<div class="collapse navbar-collapse" id="navbarToggler">
 	            <ul class="navbar-nav ml-auto">
 	                <li class="nav-item">
@@ -79,9 +79,8 @@ require('dbConnect.php');
                         <?php  echo $_SESSION['email']; ?>
                       </a>
                   </li>
-
 	            </ul>
-	        </div>
+	    </div>
 		</div>
   </nav> <!-- nav bar end-->
 
