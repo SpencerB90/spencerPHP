50d2c5ea82a78ddd4fc7fb733d8a95faf429a8b4
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-11-06 19:07:53 -0500

    test

--- followMe/examples/users.php
+++ followMe/examples/users.php
@@ -30,7 +30,7 @@ $user_id = $_SESSION['user_id'];
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 $userID = $_POST['name'];
-$followV = implode("|",$_POST['isCheck']);
+$followV = implode("|",$_POST['value']);
 
 var_dump($_POST[$followV]);
 
@@ -156,7 +156,7 @@ while($row = $following_result->fetch_row()){
 								<div class="col-md-3 col-sm-2  ml-auto mr-auto">
 									<div class="form-check">
 										<label class="form-check-label"><!--echo if checked only if followed -->
-											<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="isCheck[<?php if (in_array($row['user_id'], $fm_following_user_id)){echo "checked";}?>]" <?php if (in_array($row['user_id'], $fm_following_user_id)){echo "checked";}?> >
+											<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="<?php if (in_array($row['user_id'], $fm_following_user_id)){echo "checked";}?>" <?php if (in_array($row['user_id'], $fm_following_user_id)){echo "checked";}?> >
 											<span class="form-check-sign"></span>
 										</label>
 									</div>
