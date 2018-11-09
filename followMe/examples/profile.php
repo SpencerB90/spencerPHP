35eadd5f38a1404fa4befa9e8f55f8583c308a2a
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-11-09 10:40:03 -0500

    test

--- followMe/examples/profile.php
+++ followMe/examples/profile.php
@@ -160,13 +160,12 @@ while($row = $following_result->fetch_row()){
                        <div class="row">
                          <div class="col-md-2 col-sm-2 ml-auto mr-auto">
                          <!-- image-->	<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                         </div>
-                         <div class="col-md-7 col-sm-4  ml-auto mr-auto">
+                         
                        <!--name-->		<h6><?php echo $row['first_name'] . $row['last_name'] ; ?>
 
                        <!-- title-->	<br/><small><?php 	echo $row['title'] ; ?></small></h6>
                          </div>
-                    
+
                        </div>
 
                      <hr />
