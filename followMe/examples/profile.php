2094c5cb162f2b2abfd7d15becb8a58bf0206e72
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-11-09 10:55:46 -0500

    test

--- followMe/examples/profile.php
+++ followMe/examples/profile.php
@@ -170,7 +170,7 @@ while($row = $following_result->fetch_row()){
 
                     <div class="tab-pane text-center" id="following" role="tabpanel"><!-- list of following you-->
 
-                      <?php while($row = $result->fetch_assoc()){ ?>
+                      <?php while($row2 = $result->fetch_assoc()){ ?>
                        <div class="row">
                          <div class="col-md-2 col-sm-2 ml-auto mr-auto">
                          <!-- image-->	<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
