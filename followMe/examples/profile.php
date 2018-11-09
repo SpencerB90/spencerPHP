f841bf522738cae8c2891b3afb5f8c99909ef2db
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-11-09 10:24:45 -0500

    test

--- followMe/examples/profile.php
+++ followMe/examples/profile.php
@@ -163,50 +163,31 @@ while($row = $following_result->fetch_row()){
                 <!-- Tab panes --> <!-- on profile make following show list of following, dont need checkboxes-->
                 <div class="tab-content following"><!-- start for both-->
                     <div class="tab-pane active" id="follows" role="tabpanel"><!-- start following you-->
-                      <!--  <div class="row">
-                            <div class="col-md-6 ml-auto mr-auto">
 
-                                <ul class="list-unstyled follows">
-                                    <li>
-                                        <div class="row">
-                                            <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-                                                <img src="../assets/img/faces/clem-onojeghuo-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                                            </div>
-                                            <div class="col-md-7 col-sm-4  ml-auto mr-auto">
-                                                <h6>Flume<br/><small>Musical Producer</small></h6>
-                                            </div>
-                                            <div class="col-md-3 col-sm-2  ml-auto mr-auto">
-												                    <div class="form-check">
-					                                <label class="form-check-label">
-					                                    <input class="form-check-input" type="checkbox" value="" checked>
-					                                    <span class="form-check-sign"></span>
-					                                </label>
-					                            </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <hr />
-                                    <li>
-                                        <div class="row">
-                                            <div class="col-md-2 ml-auto mr-auto ">
-                                                <img src="../assets/img/faces/ayo-ogunseinde-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                                            </div>
-                                            <div class="col-md-7 col-sm-4">
-                                                <h6>Banks<br /><small>Singer</small></h6>
-                                            </div>
-                                            <div class="col-md-3 col-sm-2">
-												                 <div class="form-check">
-					                                <label class="form-check-label">
-					                                    <input class="form-check-input" type="checkbox" value="">
-					                                    <span class="form-check-sign"></span>
-					                                </label>
-					                            </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>-->
+                      <?php while($row2 = $result->fetch_assoc()){ ?>
+
+                       <div class="row">
+                         <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+                         <!-- image-->	<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                         </div>
+                         <div class="col-md-7 col-sm-4  ml-auto mr-auto">
+                       <!--name-->		<h6><?php echo $row['first_name'] . $row['last_name'] ; ?>
+
+                       <!-- title-->	<br/><small><?php 	echo $row['title'] ; ?></small></h6>
+                         </div>
+                         <div class="col-md-3 col-sm-2  ml-auto mr-auto">
+                           <div class="form-check">
+                             <label class="form-check-label"><!--echo if checked only if followed -->
+                              <input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $fm_user_id)){echo "checked";}?> >
+                               <span class="form-check-sign"></span>
+                             </label>
+                           </div>
+                         </div>
+                       </div>
+
+                     <hr />
+                   <?php } ?>
+
                     </div> <!-- end following you-->
 
                     <div class="tab-pane text-center" id="following" role="tabpanel"><!-- list of following you-->
