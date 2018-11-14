4001636ae810b4b7472a8818c367a4955f15a673
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-11-14 17:11:29 -0500

    test

--- followMe/examples/imageupload.php
+++ followMe/examples/imageupload.php
@@ -64,6 +64,8 @@ if ($uploadVerify) {
 
   $sql = "UPDATE fm_users set image_url = \"$target_file\" where email = '$email'";
   $conn->query($sql);
+
+  $_SESSION['image_url'] = $target_file;
  ?>
 
 
