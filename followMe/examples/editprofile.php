02fde5c17c43432bfd302bfe31a0e5b83c531341
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-11-20 22:27:18 -0500

    test

--- followMe/examples/editprofile.php
+++ followMe/examples/editprofile.php
@@ -115,7 +115,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
                           <!-- edit upload image for profile, /images/userid.jpg // option to edit img-->
                           <div class="col-md-6">
-                              <a href="imageupload.php">Click to Edit Profile Picture</a>
+                              <a href="imageupload.php">Click Here to Edit Profile Picture</a>
                               <div class="input-group">
                             <img src="<?php  echo  $_SESSION['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
                             </div>
